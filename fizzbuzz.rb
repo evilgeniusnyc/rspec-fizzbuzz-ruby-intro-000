1f3cff934661670f1573b8a13cdb8763a4f19b0e
Steven Williams <evilgeniusnyc@me.com>
2020-02-28 00:39:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,9 +7,9 @@ def fizzbuzz(int)
   end
 
   if int % 5 == 0 # if the number int is divisible by 5 
-     "Buzz" # Go Buzz
+     return "Buzz" # Go Buzz
   end
   if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 5 and 3 
-  "fizzbuzz"
+    return "fizzbuzz"
   end
 end
